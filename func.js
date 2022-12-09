6edf9d602d0b31b5fbe02793d02e9c87ff19820c
SophiePBH <88929359+SophiePBH@users.noreply.github.com>
2022-12-09 11:59:47 +0100

    masse kule greier som jeg ikke husker

--- func.js
+++ func.js
@@ -1,5 +1,6 @@
 window.onload = start
-function start() {
+function start() 
+{
     //Buttons
     const guessButton = document.getElementById("guessbtn");
     guessButton.onclick = guess;
@@ -8,57 +9,82 @@ function start() {
     const hintButton = document.getElementById("hintbtn");
     hintButton.onclick = showHint;
 
+    document.getElementsByClassName("restart").onclick = newGame;
     //Attempts
-    var attemptsTxt = document.getElementById("attemptsTxt");
     var attempts = 10;
+    var attemptsTxt = document.getElementById("attemptsTxt");
+    attemptsTxt.innerHTML = attempts;
 
     //Hints
     var hintAmount = 3;
     var hint =  "Gjett et tall først!";
-    var hintTxt = document.getElementById("hint").innerHTML = hintAmount;
+    var hintTxt = document.getElementById("hint");
+    hintTxt.innerHTML = hintAmount;
 
     //Numbers
     var randomNumber = (Math.floor(Math.random() * 100) + 1);
     var guessedNumber;
 
-    function guess() {
+    function guess() 
+    {
+        document.getElementById("number").placeholder = "";
         guessedNumber = document.getElementById("number").value;
-        if (attempts > 0) {
+        if (attempts > 1 && document.getElementById("number").value != "") 
+        {
+            document.getElementById("number").value = "";
             if (guessedNumber > randomNumber) {
                 attempts--;
-                hint = "Tallet er lavere";
-                document.getElementById("Error").innerHTML = "Tallet ditt var for høyt! Du har " + attempts + " forsøk igjen!";
+                attemptsTxt.innerHTML = attempts;
+                hint = "Tallet ditt er for høyt!";
             }
             else if (guessedNumber < randomNumber) {
                 attempts--;
-                hint = "Tallet er høyere";
-                document.getElementById("Error").innerHTML = "Tallet ditt var for lavt! Du har " + attempts + " forsøk igjen!";
+                attemptsTxt.innerHTML = attempts;
+                hint = "Tallet ditt er for lavt!";
             }
             else {
-                document.getElementById("result").innerHTML = "Du gjettet riktig! Bra jobba!!!!!!!!!!";
+                attempts--;
+                attemptsTxt.innerHTML = attempts;
+                document.getElementById("result1").innerHTML = "Du gjettet riktig! Bra jobba!!!!!!!!!!";
                 document.getElementById('modalWin').style.display='block';
             }
 
             console.log(guessedNumber);
             console.log(randomNumber);
         }
-        else {
-            document.getElementById("result").innerHTML = "Game over!";
+        else if(document.getElementById("number").value == "")
+        {
+            document.getElementById("number").placeholder = "Du må skrive et tall!";
+        }
+        else 
+        {
+            attempts--;
+            attemptsTxt.innerHTML = attempts;
+            document.getElementById("result2").innerHTML = "Game over!";
             document.getElementById('modalFail').style.display='block';
         }
     }
 
-    function showHint() {
-        if (hintAmount > 3) {
-            document.getElementById("hint").innerHTML = hint;
-            hintAmount--;
+    function showHint() 
+    {
+        if(document.getElementById("number").value == "") {
+            if (hintAmount > 0) {
+                document.getElementById("number").placeholder = hint;
+                hintAmount--;
+                hintTxt.innerHTML = hintAmount;
+            }
+            else 
+            {
+                document.getElementById("number").placeholder = "Du har ikke flere hint igjen";
+            }
         }
         else {
-            document.getElementById("hint").innerHTML = "Du har ikke flere hint igjen";
+            document.getElementById("number").placeholder = "Du må skrive et tall!";
         }
     }
 
-    function newGame() {
+    function newGame() 
+    {
         randomNumber = (Math.floor(Math.random() * 100) + 1);
         attempts = 10;
         hintAmount = 3;

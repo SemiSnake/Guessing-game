cb76cc3e73169fec26ad47de1d3a52a1b65edf63
SemiSnake <tohja001@gmail.com>
2022-12-09 09:29:43 +0100

    new

--- func.js
+++ func.js
@@ -10,19 +10,23 @@ function start() {
     guessButton.onclick = guess;
     var randomNumber;
     var guessedNumber;
+    randomNumber = (Math.floor(Math.random() * 100) + 1)
     var attempts = 10;
-    function guess() {
+    function guess() 
+    {
         guessedNumber = document.getElementById("number").value;
         if (guessedNumber > randomNumber) {
             attempts--;
-            document.getElementById("Error").innerHTML = "Tallet ditt var for høyt! Du har " + attempts + " forsøk igjen!";
+            console.log("Lavt");
+            //document.getElementById("Error").innerHTML = "Tallet ditt var for høyt! Du har " + attempts + " forsøk igjen!";
         }
         else if (guessedNumber < randomNumber) {
             attempts--
-            document.getElementById("Error").innerHTML = "Tallet ditt var for lavt! Du har " + attempts + " forsøk igjen!";
+            console.log("høyt");
+            //document.getElementById("Error").innerHTML = "Tallet ditt var for lavt! Du har " + attempts + " forsøk igjen!";
         }
         else {
-            document.getElementById("Error").innerHTML = "Du gjettet riktig! Bra jobba!!!!!!!!!!";
+            //document.getElementById("Error").innerHTML = "Du gjettet riktig! Bra jobba!!!!!!!!!!";
         }
         console.log(guessedNumber);
         console.log(randomNumber);
